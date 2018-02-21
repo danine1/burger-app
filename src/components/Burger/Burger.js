145900beea685a5b4ebed21e62aed663685000c2
Ronan Dineen <ronan@ematix19.ematix>
2018-02-21 09:30:04 +0100

    built burger builder components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map( igKey => {
             //transforming an object of key/value pairs into an array
             //(key is number of ingredients and value is type of ingredient)
             return [...Array( props.ingredients[igKey] )].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        //map and reduce are default array functions
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
