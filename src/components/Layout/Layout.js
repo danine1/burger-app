23f1441e924b64bc55529f970d61bd2572325ff2
Ronan Dineen <ronan@ematix19.ematix>
2018-02-22 15:22:08 +0100

    added sidebar toggle and styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-);
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !this.state.showSideDrawer }
+        });
+    }
+
+    render () {
+        return(
+        <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        );
+    }
+};
+
+export default Layout;
